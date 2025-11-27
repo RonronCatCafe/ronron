c80d390173764518992a26acef4c504db77b01fc
RonronCatCafe <greice@ronroncatcafe.com>
2025-11-27 13:12:25 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'ronron-pink': '#E8B4C8',
+        'ronron-pink-light': '#F5D9E4',
+        'ronron-black': '#1A1A1A',
+        'ronron-cream': '#FFF9F5',
+      }
+    }
+  }
+}
